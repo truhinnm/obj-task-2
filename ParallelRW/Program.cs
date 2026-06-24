45ecfa3ef61c2a0ce93513d693bf96bf4f6f687f
trushinnm <nick_trush@mail.ru>
2026-06-24 17:49:58 +0300

    Solution

---
+++ ParallelRW/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ParallelRW
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var tasks = new Task[110];
+
+            // 100 читателей
+            for (int i = 0; i < 100; i++)
+            {
+                int id = i;
+
+                tasks[i] = Task.Run(async () =>
+                {
+                    for (int j = 0; j < 50; j++)
+                    {
+                        var value = StaticServer.GetCount();
+
+                        Console.WriteLine(
+                            $"Reader {id}: {value}");
+
+                        await Task.Delay(Random.Shared.Next(10, 50));
+                    }
+                });
+            }
+
+            // 10 писателей
+            for (int i = 0; i < 10; i++)
+            {
+                int id = i;
+
+                tasks[100 + i] = Task.Run(async () =>
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        StaticServer.AddToCount(1);
+
+                        Console.WriteLine(
+                            $"Writer {id}: +1");
+
+                        await Task.Delay(Random.Shared.Next(100, 300));
+                    }
+                });
+            }
+
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine();
+            Console.WriteLine($"Final count = {StaticServer.GetCount()}");
+        }
+    }
+}
---
+++ ParallelRW/StaticServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParallelRW
+{
+    public static class StaticServer
+    {
+        private static int count = 0;
+
+        private static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+
+        public static int GetCount()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+
+        }
+
+        public static void AddToCount(int value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                count += value;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+    }
+}
